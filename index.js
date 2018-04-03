4a3bd671a1c466002e98b723f800bb22f6cfd02d
Vin Busquet <computationalcore@gmail.com>
2018-04-02 21:51:40 -0300

    feat: Use Header at App component

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
+// Render the App component to the device
 AppRegistry.registerComponent(appName, () => App);
