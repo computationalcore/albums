0a6e9ece5200a07116ff4e1c7ab295121f3520a4
Vin Busquet <computationalcore@gmail.com>
2018-04-05 21:03:26 -0300

    feat: Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottonWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+  }
+};
+
+const Card = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+export default Card;
