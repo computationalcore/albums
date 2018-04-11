5e9788138da51007dad5555dadd2bf003fc394cb
Vin Busquet <computationalcore@gmail.com>
2018-04-11 02:49:19 -0300

    refactor: Remove warnings

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+console.disableYellowBox = true;
+
 // Render the App component to the device
 AppRegistry.registerComponent(appName, () => App);
